f965ddd5196d0325405abe03bd049e6fa044b017
ramirols <ramirols.dev@gmail.com>
2026-01-07 15:22:23 -0500

    feat: implement root layout with Geist fonts, global styles, Toaster, and Content-Security-Policy.

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,55 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="
+            default-src 'self';
+            script-src
+              'self'
+              'unsafe-inline'
+              'unsafe-eval'
+              https://js.stripe.com
+              https://m.stripe.network
+              https://*.stripe.com
+              https://geoissuer.cardinalcommerce.com
+              https://*.cardinalcommerce.com
+              https://*.online-metrix.net;
+
+            frame-src
+              https://js.stripe.com
+              https://hooks.stripe.com
+              https://geoissuer.cardinalcommerce.com
+              https://*.cardinalcommerce.com
+              https://*.stripe.com;
+
+            connect-src
+              'self'
+              https://api.stripe.com
+              https://stripe.com
+              https://hooks.stripe.com
+              https://m.stripe.com
+              https://m.stripe.network
+              https://r.stripe.com
+              https://*.stripe.com
+              https://*.online-metrix.net
+              https://*.cardinalcommerce.com
+              https://*.supabase.co;
+
+            img-src
+              'self'
+              data:
+              https://*.stripe.com;
+
+            style-src
+              'self'
+              'unsafe-inline'
+              https://js.stripe.com
+              https://*.stripe.com;
+          "
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
