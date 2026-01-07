2c79e497b8317dee31269a37cea43ba5afabde9c
ramirols <ramirols.dev@gmail.com>
2026-01-07 14:09:33 -0500

    :tada: configurando next.config.ts para el modo solicitado

--- next.config.ts
+++ next.config.ts
@@ -10,10 +10,23 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' https://js.stripe.com;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: https://*.stripe.com;
-              frame-src https://js.stripe.com https://hooks.stripe.com;
+              
+              script-src
+                'self'
+                'unsafe-inline'
+                https://js.stripe.com
+                https://m.stripe.network
+                https://*.stripe.com
+                https://geoissuer.cardinalcommerce.com
+                https://*.cardinalcommerce.com
+                https://*.online-metrix.net;
+
+              frame-src
+                https://js.stripe.com
+                https://hooks.stripe.com
+                https://geoissuer.cardinalcommerce.com
+                https://*.cardinalcommerce.com;
+
               connect-src
                 'self'
                 https://api.stripe.com
@@ -22,7 +35,19 @@ const nextConfig: NextConfig = {
                 https://m.stripe.com
                 https://m.stripe.network
                 https://r.stripe.com
+                https://*.stripe.com
+                https://*.online-metrix.net
+                https://*.cardinalcommerce.com
                 https://*.supabase.co;
+
+              img-src
+                'self'
+                data:
+                https://*.stripe.com;
+
+              style-src
+                'self'
+                'unsafe-inline';
             `.replace(/\s{2,}/g, " ").trim(),
           },
         ],
